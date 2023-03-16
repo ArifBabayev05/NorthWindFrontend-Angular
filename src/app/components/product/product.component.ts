f0449f8d0f3b9767064bcddf6075910e971b8cb3
Arif Babayev <arifrb@code.edu.az>
2023-03-16 11:08:00 +0400

    Getting Datas form API

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import {HttpClient} from "@angular/common/http"
+import { productResponceModel } from 'src/app/models/productResponseModel';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
 
-  products:Product[] = []
+  products: Product[] = [];
+  apiUrl = "https://localhost:20232/api/product/getall"
+  productResponseModel : productResponceModel={
+    data:this.products,
+    success:true,
+    message: ""
+  }
+
+  constructor(private httpClient:HttpClient){}
+  ngOnInit(): void {
+    this.getProduct();
+  }
+
+  getProduct(){
+    this.httpClient.get<productResponceModel>(this.apiUrl)
+    .subscribe((res)=>{
+      this.products = res.data;
+    })
+  }
 }
